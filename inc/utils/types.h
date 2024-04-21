58cb1f0287a090cc04ddc876362b1e7006c0ff62
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2024-04-21 18:47:05 +0200

    Add CMSIS-DSP submodule. Fix DSP instructions usage.

--- inc/utils/types.h
+++ inc/utils/types.h
@@ -5,6 +5,10 @@
  *      Author: Ludo
  */
 
+#include "stddef.h"
+#include "stdint.h"
+#include "arm_math_types.h"
+
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
@@ -13,23 +17,6 @@
  *******************************************************************/
 
 typedef char				char_t;
-
-typedef signed char			int8_t;
-typedef unsigned char		uint8_t;
-
-typedef signed short		int16_t;
-typedef unsigned short		uint16_t;
-
-typedef signed int			int32_t;
-typedef unsigned int		uint32_t;
-
-typedef signed long long	int64_t;
-typedef unsigned long long	uint64_t;
-
-typedef float				float32_t;
-typedef double				float64_t;
-
-#define NULL				((void*) 0)
 
 #define UNUSED(x)			((void) x)
 
